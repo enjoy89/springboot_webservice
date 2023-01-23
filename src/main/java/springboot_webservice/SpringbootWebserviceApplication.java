921f66fbc16603650f7cc05b02ace15c1575a3f9
jeondui <jeondui@gmail.com>
2023-01-23 18:39:19 +0900

    Initial commit

---
+++ src/main/java/springboot_webservice/SpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package springboot_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebserviceApplication.class, args);
+	}
+
+}
